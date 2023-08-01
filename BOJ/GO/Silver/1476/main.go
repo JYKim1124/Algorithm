68b4a11b9b605951ab9c5f2dc8c2391893393051
Juyeon1124 <Johnszone@ewhain.net>
2023-08-01 16:44:59 +0900

    feat : 1476

---
+++ BOJ/GO/Silver/1476/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var E, S, M int
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var year int
+	year = 1
+
+	fmt.Fscanln(reader, &E, &S, &M)
+
+	for {
+		if (year-E)%15 == 0 && (year-S)%28 == 0 && (year-M)%19 == 0 {
+			break
+		}
+		year++
+	}
+
+	fmt.Fprintln(writer, year)
+}
